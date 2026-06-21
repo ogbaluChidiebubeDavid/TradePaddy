ec9f7f113b9a6a54eb29c07d06894316f548d214
ogbaluChidiebubeDavid <ogbaluchukwudi5@gmail.com>
2026-06-21 16:56:43 +0100

    Configure database and add Render blueprint

--- test_db.js
+++ test_db.js
@@ -2,10 +2,10 @@ import pg from 'pg';
 const { Client } = pg;
 
 async function run() {
-  const connectionString = "postgresql://neondb_owner:npg_uJOL7jTNPFt0@ep-cold-salad-apuu75vl.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-  console.log('Connecting to:', connectionString);
+  const connectionString = process.env.DATABASE_URL || "postgresql://neondb_owner:YOUR_PASSWORD_HERE@ep-cold-salad-apuu75vl-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+  console.log('Connecting to database...');
   const client = new Client({ connectionString });
-  
+
   try {
     await client.connect();
     console.log('Connected!');
